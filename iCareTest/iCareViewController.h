b0931efab7cd296c101187ab72174d290327aca3
Kevin Lee <Kevin@riomoutekiMac.local>
2013-05-16 09:40:45 +0800

    Initial Commit

---
+++ iCareTest/iCareViewController.h
@@ -0,0 +1,13 @@
+//
+//  iCareViewController.h
+//  iCareTest
+//
+//  Created by Kevin Lee on 13-5-16.
+//  Copyright (c) 2013年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCareViewController : UIViewController
+
+@end
---
+++ iCareTest/iCareViewController.m
@@ -0,0 +1,29 @@
+//
+//  iCareViewController.m
+//  iCareTest
+//
+//  Created by Kevin Lee on 13-5-16.
+//  Copyright (c) 2013年 Kevin. All rights reserved.
+//
+
+#import "iCareViewController.h"
+
+@interface iCareViewController ()
+
+@end
+
+@implementation iCareViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
